3c68d1cd5dcbf143e1f88148ce2b7bba9d318ec8
Desquita Sasyili <sasyilikheyla@gmai.com>
2024-09-20 15:37:50 +0700

    B

---
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -0,0 +1,70 @@
+package com.example.lab_week_04
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailFragment : Fragment() {
+    private val coffeeTitle: TextView?
+        get() = view?.findViewById(R.id.coffee_title)
+    private val coffeeDesc: TextView?
+        get() = view?.findViewById(R.id.coffee_desc)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+    fun setCoffeeData(id: Int){
+        when(id){
+            R.id.affogato -> {
+                coffeeTitle?.text = getString(R.string.affogato_title)
+                coffeeDesc?.text = getString(R.string.affogato_desc)
+            }
+            R.id.americano -> {
+                coffeeTitle?.text = getString(R.string.americano_title)
+                coffeeDesc?.text = getString(R.string.americano_desc)
+            }
+            R.id.latte -> {
+                coffeeTitle?.text = getString(R.string.latte_title)
+                coffeeDesc?.text = getString(R.string.latte_desc)
+            }
+            R.id.hazelnut -> {
+                coffeeTitle?.text = getString(R.string.hazelnut_title)
+                coffeeDesc?.text = getString(R.string.hazelnut_desc)
+            }
+            R.id.caramel -> {
+                coffeeTitle?.text = getString(R.string.caramel_title)
+                coffeeDesc?.text = getString(R.string.caramel_desc)
+            }
+            R.id.hot -> {
+                coffeeTitle?.text = getString(R.string.caramel_title)
+                coffeeDesc?.text = getString(R.string.hot_desc)
+            }
+        }
+    }
+    companion object {
+        private const val COFFEE_ID = "COFFEE_ID"
+    }
+}
